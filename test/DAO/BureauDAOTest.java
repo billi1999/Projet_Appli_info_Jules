480b8110bd925ed3a90c21d08bc3d39f212c8036
billibob <julesblothiaux@gmail.com>
2019-04-14 13:16:54 +0200

    Mise en place de tout les test excepté testReadSigle

--- test/DAO/BureauDAOTest.java
+++ test/DAO/BureauDAOTest.java
@@ -13,6 +13,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import Creation_Objet.Bureau;
+import java.sql.SQLException;
 import myconnections.DBConnection;
 
 /**
@@ -65,7 +66,6 @@ public class BureauDAOTest {
        BureauDAO instance=new BureauDAO();
        instance.setConnection(dbConnect);
        Bureau expResult=new Bureau(0,"F0","000000000","Test");
-   
        Bureau result=instance.create(obj);
         System.out.println("Valeur de l'id expresult : "+expResult.getIdbur());
         System.out.println("Valeur de l'id result :"+result.getIdbur());
@@ -83,7 +83,20 @@ public class BureauDAOTest {
      */
     @Test
     public void testUpdate() throws Exception {
-       
+        System.out.println("update");
+        Bureau obj=new Bureau(0,"F0","000000000","Test");
+        BureauDAO instance=new BureauDAO();
+       instance.setConnection(dbConnect);
+       obj=instance.create(obj);
+       obj.setSigle("F02");
+       obj.setTel("1");
+       obj.setDescription("Test2");
+       Bureau expResult=obj;
+       Bureau result=instance.update(obj);
+       assertEquals("Sigle différents",expResult.getSigle(),result.getSigle());
+       assertEquals("Tel différents",expResult.getTel(),result.getTel());
+       assertEquals("Description différentes",expResult.getDescription(),result.getDescription());
+       instance.delete(result);
     }
 
     /**
@@ -91,7 +104,20 @@ public class BureauDAOTest {
      */
     @Test
     public void testDelete() throws Exception {
+        System.out.println("delete");
+        Bureau obj=new Bureau(0,"F0","000000000","Test");
+        BureauDAO instance=new BureauDAO();
+       instance.setConnection(dbConnect);
+       obj=instance.create(obj);
+       instance.delete(obj);
+       try {
+            instance.read(obj.getIdbur());
+            fail("exception de record introuvable non générée");
+            //si la suppression est bien effectuée
+        }
+        catch(SQLException e){}
        
+        
     }
 
     /**
@@ -99,7 +125,27 @@ public class BureauDAOTest {
      */
     @Test
     public void testRead() throws Exception {
+        System.out.println("read");
+        int idbur=0;
+        BureauDAO instance=new BureauDAO();
+        instance.setConnection(dbConnect);
+        Bureau obj=new Bureau(0,"F0","000000000","Test");
+        Bureau expResult=instance.create(obj);
+        idbur=expResult.getIdbur();
+        Bureau res=instance.read(idbur);
         
+       assertEquals("Sigle différents",expResult.getSigle(),res.getSigle());
+       assertEquals("Tel différents",expResult.getTel(),res.getTel());
+       assertEquals("Description différentes",expResult.getDescription(),res.getDescription());
+   
+       assertEquals("idbur différents",expResult.getIdbur(),res.getIdbur());
+       try{
+            res=instance.read(0);
+            fail("exception d'id inconnu non générée");
+            
+        }
+        catch(SQLException e){}
+       instance.delete(res);
     }
 
     /**
@@ -107,7 +153,21 @@ public class BureauDAOTest {
      */
     @Test
     public void testRechDescription() throws Exception {
-       
+        System.out.println("RechDescription");
+         Bureau obj1=new Bureau(0,"F0","000000000","Test");
+         Bureau obj2=new Bureau(0,"F15","456789787","Test");
+         String desc="Test";
+         BureauDAO instance=new BureauDAO();
+         instance.setConnection(dbConnect);
+         obj1=instance.create(obj1);
+         obj2=instance.create(obj2);
+         
+         List<Bureau> res=instance.rechDescription(desc);
+         if(res.indexOf(obj1)<0) fail("record introuvable "+obj1);//return -1 when doesn't found
+         if(res.indexOf(obj2)<0) fail("record introuvable"+obj2);
+        instance.delete(obj1);
+        instance.delete(obj2);
+         
     }
     
 }
