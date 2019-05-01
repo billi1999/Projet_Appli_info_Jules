68ea198c17461da61653c0b830a512a404a3f31a
billibob <julesblothiaux@gmail.com>
2019-05-01 14:43:21 +0200

    Mise à jour de la documentation du bureau

--- src/Creation_Objet/Bureau.java
+++ src/Creation_Objet/Bureau.java
@@ -19,7 +19,12 @@ public class Bureau {
     protected String description;
 
     
-
+/**
+ * fonction qui va vérifier si l'objet bureau est fonctionnel au travers de test
+ * @param obj
+ * @return booléen
+ *  
+ */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -46,49 +51,85 @@ public class Bureau {
         }
         return true;
     }
-
+/**
+ * Constructeur vide du bureau
+ */
     public Bureau() {
     }
-
+/**
+ * 
+ * @param idbur
+ * @param sigle
+ * @param tel
+ * @param description 
+ * constructeur remplit du bureau
+ */
     public Bureau(int idbur, String sigle, String tel, String description) {
         this.idbur = idbur;
         this.sigle = sigle;
         this.tel = tel;
         this.description = description;
     }
-
+/**fonction pour obtenir l'identité du bureau
+ * 
+ * @return l'id du bureau
+ */
     public int getIdbur() {
         return idbur;
     }
-
+/**fonction pour obtenir le sigle du bureau
+ * 
+ * @return le sigle du bureau
+ */
     public String getSigle() {
         return sigle;
     }
-
+/**fonction pour obtenir le téléphone du bureau
+ * 
+ * @return le numéro de téléphone
+ */
     public String getTel() {
         return tel;
     }
-
+/**fonction pour obtenir la description du bureau
+ * 
+ * @return la description
+ */
     public String getDescription() {
         return description;
     }
-
+/**méthode pour mettre à jour l'id du bureau
+ * 
+ * @param idbur 
+ */
     public void setIdbur(int idbur) {
         this.idbur = idbur;
     }
-
+/**méthode pour mettre à jour le Sigle du bureau
+ * 
+ * @param sigle 
+ */
     public void setSigle(String sigle) {
         this.sigle = sigle;
     }
-
+/**méthode pour mettre à jour le téléphone du bureau
+ * 
+ * @param tel 
+ */
     public void setTel(String tel) {
         this.tel = tel;
     }
-
+/**méthode pour mettre à jour la description
+ * 
+ * @param description 
+ */
     public void setDescription(String description) {
         this.description = description;
     }
-
+/**méthode pour afficher le contenu de l'objet
+ * 
+ * @return 
+ */
     @Override
     public String toString() {
         return "Bureau{" + "idbur=" + idbur + ", sigle=" + sigle + ", tel=" + tel + ", description=" + description + '}';
