7a2662e51f4e1928b8dd75c9dd6c543c08a2006b
billibob <julesblothiaux@gmail.com>
2019-05-03 13:58:38 +0200

    Création constructeur Employe

--- src/Creation_Objet/Employe.java
+++ src/Creation_Objet/Employe.java
@@ -1,11 +1,22 @@
 
 package Creation_Objet;
 
+import java.util.Objects;
+
 public class Employe {
-    private int idemp;
-    private String Matricule;
-    private String nom;
-    private String prenom;
+    protected int idemp;
+    protected String Matricule;
+    protected String nom;
+    protected String prenom;
+    protected int idbur;
+
+    public Employe(int idemp, String Matricule, String nom, String prenom, int idbur) {
+        this.idemp = idemp;
+        this.Matricule = Matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.idbur = idbur;
+    }
 
     
     
@@ -40,6 +51,52 @@ public class Employe {
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
+
+    public int getIdbur() {
+        return idbur;
+    }
+
+    public void setIdbur(int idbur) {
+        this.idbur = idbur;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employe other = (Employe) obj;
+        if (this.idemp != other.idemp) {
+            return false;
+        }
+        if (this.idbur != other.idbur) {
+            return false;
+        }
+        if (!Objects.equals(this.Matricule, other.Matricule)) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.prenom, other.prenom)) {
+            return false;
+        }
+        return true;
+    }
+
     
+    @Override
+    public String toString() {
+        return "Employe{" + "idemp=" + idemp + ", Matricule=" + Matricule + ", nom=" + nom + ", prenom=" + prenom + ", idbur=" + idbur + '}';
+    }
+
+    
+
     
 }
