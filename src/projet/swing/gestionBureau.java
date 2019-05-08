b0930dcd246691d404483bda40a49dca6df6774e
billibob <julesblothiaux@gmail.com>
2019-05-08 22:56:01 +0200

    mise en place du swing pour la table bureau fin

--- src/projet/swing/gestionBureau.java
+++ src/projet/swing/gestionBureau.java
@@ -33,7 +33,6 @@ public class gestionBureau extends javax.swing.JFrame {
     burDAO.setConnection(dbConnect);
     
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
