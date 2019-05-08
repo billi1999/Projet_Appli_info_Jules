047bd3540d94406de506373b2db1acde5a80f4ac
billibob <julesblothiaux@gmail.com>
2019-05-08 22:55:25 +0200

    Mise en place de la gestion des employés

--- src/projet_jules/Projet_Jules.java
+++ src/projet_jules/Projet_Jules.java
@@ -1,215 +1,370 @@
-
 package projet_jules;
 
 import java.sql.*;
 import myconnections.DBConnection;
 import java.util.*;
 import Creation_Objet.Bureau;
+import Creation_Objet.Employe;
 import DAO.BureauDAO;
-
-
-
-
+import DAO.EmployeDAO;
 
 /**
  *
  * @author Jules
  */
 public class Projet_Jules {
+
     //Bureau bur=new Bureau();
-     Bureau bur=null;
-    
+    Bureau bur = null;
+    Employe emp = null;
+
     private BureauDAO BureauDAO;
-Scanner sc = new Scanner(System.in);
- Connection dbConnect = DBConnection.getConnection();
- 
+    private EmployeDAO EmployeDAO;
+    Scanner sc = new Scanner(System.in);
+    Connection dbConnect = DBConnection.getConnection();
 
- 
- public void menuDebut() throws SQLException{
-    String rep="";
-    int choix;
-    if(dbConnect==null){
-        System.out.print("Problème de connexion à la base de donnée");
-        
-        System.exit(1);
-    }
-    BureauDAO =new BureauDAO();
-    BureauDAO.setConnection(dbConnect);
-    do{
-        do{
-            
-            System.out.println("\nEntrer une valeur pour :\n\t1.Gérer les bureaux\n\t4. Quitter l'application");
-            System.out.print("Choix :");
-           rep=sc.nextLine();
-          
-        }while(verifier_chaine(rep,"[1-4]")==false);//mettre [0-4]
-        choix=Integer.parseInt(rep);
-        switch(choix){
-            
-            case 1: 
-                
-                menuBureau();
-                break;
-            
-            case 4:break;
+    public void menuDebut() throws SQLException {
+        String rep = "";
+        int choix;
+        if (dbConnect == null) {
+            System.out.print("Problème de connexion à la base de donnée");
+
+            System.exit(1);
         }
-    }while(choix!=4);
-    
-    
-}
+        BureauDAO = new BureauDAO();
+        EmployeDAO = new EmployeDAO();
+        BureauDAO.setConnection(dbConnect);
+        EmployeDAO.setConnection(dbConnect);
+        do {
+            do {
 
+                System.out.println("\nEntrer une valeur pour :\n\t1.Gérer les bureaux\n\t2.Gérer les employés\n\t4. Quitter l'application");
+                System.out.print("Choix :");
+                rep = sc.nextLine();
 
-public boolean verifier_chaine(String chaine,String regexp){
-    return chaine.matches(regexp);
-}
-public void menuBureau() throws SQLException{
-    String rep="";
-    int choix;
-    do{
-            
+            } while (verifier_chaine(rep, "[1-4]") == false);//mettre [0-4]
+            choix = Integer.parseInt(rep);
+            switch (choix) {
+
+                case 1:
+
+                    menuBureau();
+                    break;
+                case 2:
+                    menuEmploye();
+                    break;
+                case 3:
+                    modifierEmploye();
+                    break;
+                case 4:
+                    break;
+            }
+        } while (choix != 4);
+
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public boolean verifier_chaine(String chaine, String regexp) {
+        return chaine.matches(regexp);
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void menuBureau() throws SQLException {
+        String rep = "";
+        int choix;
+        do {
+
             System.out.print("\nEntrer une valeur pour :"
                     + "\n\t1.Créer un bureaux"
                     + "\n\t2.Rechercher par sigle(supprimer + maj)"
                     + "\n\t3.Recherche sur base de la description"
                     + "\n\t4.Modifier les éléments d'un sigle"
                     + "\nChoix :");
-                    
-           rep=sc.nextLine();
-        }while(verifier_chaine(rep,"[1-5]")==false);
-    choix=Integer.parseInt(rep);
-    switch(choix){
-        case 1: creationBureau();break;
-        case 2: MenuRechercherSigle() ;
+
+            rep = sc.nextLine();
+        } while (verifier_chaine(rep, "[1-5]") == false);
+        choix = Integer.parseInt(rep);
+        switch (choix) {
+            case 1:
+                creationBureau();
                 break;
-        case 3:rechercherDesc();break;
-        case 4:modifierBureau();break;
-        
-        
-        default:break;
+            case 2:
+                MenuRechercherSigle();
+                break;
+            case 3:
+                rechercherDesc();
+                break;
+            case 4:
+                modifierBureau();
+                break;
+
+            default:
+                break;
+        }
+
     }
-    
-}
+////////////////////////////////////////////////////////////////////////////
+
+    public void menuEmploye() throws SQLException {
+        String rep = "";
+        int choix;
+        do {
+            System.out.println("Entrer une valeur pour :"
+                    + "\n\t1.Créer un Employé"
+                    + "\n\t2.Rechercher par idemp(supprimer + maj)"
+                    + "\n\t3.Modifier un employé"
+                    + "\nChoix :");
+            rep = sc.nextLine();
+        } while (verifier_chaine(rep, "[1-5]") == false);
+        choix = Integer.parseInt(rep);
+        switch (choix) {
+            case 1:
+                creationEmploye();
+                break;
+            case 2:
+                MenuRechercherIdemp();
+                break;
+               
+            case 3:
+                modifierEmploye();
+                break;
+            default:
+                break;
+
+        }
+    }
 ////////////////////////////////////////////////////////////////////////////
 //création avec affichage de l'id résultant
-public void creationBureau(){
-    System.out.print("Sigle :");
-    String sigle=sc.nextLine();
-    System.out.print("Tel :");
-    String tel=sc.nextLine();
-    System.out.print("Description :");
-    String description=sc.nextLine();
-    bur=new Bureau(0,sigle,tel,description);
-    //System.out.println(bur);
-    try{
-      bur=BureauDAO.create(bur);
-      System.out.println("\n\tLa valeur de idbur :"+bur.getIdbur()+
-              "\n\tSigle :"+bur.getSigle()+
-              "\n\tTel :"+bur.getTel()+
-              "\n\tDescription :"+bur.getDescription());
-    }catch(SQLException e){
-        
-        System.out.println("Erreur de création(creationBureau) :"+e);
+
+    public void creationBureau() {
+        System.out.print("Sigle :");
+        String sigle = sc.nextLine();
+        System.out.print("Tel :");
+        String tel = sc.nextLine();
+        System.out.print("Description :");
+        String description = sc.nextLine();
+        bur = new Bureau(0, sigle, tel, description);
+        try {
+            bur = BureauDAO.create(bur);
+            System.out.println("\n\tLa valeur de idbur :" + bur.getIdbur()
+                    + "\n\tSigle :" + bur.getSigle()
+                    + "\n\tTel :" + bur.getTel()
+                    + "\n\tDescription :" + bur.getDescription());
+        } catch (SQLException e) {
+
+            System.out.println("Erreur de création(creationBureau) :" + e);
+        }
     }
-}
+///////////////////////////////////////////////////////////////////////////
 
-public void rechercherDesc(){
-    System.out.println("Cherchez un bureau sur base de la description :");
-    
-    String desc=sc.nextLine();
-   try{
-       List <Bureau> liste=BureauDAO.rechDescription(desc);
-       for (Bureau k: liste){
-           System.out.println("Idbur :"+k.getIdbur()+""
-                   + "\tSigle :"+k.getSigle()+""
-                   + "\tTel :"+k.getTel()+""
-                   + "\tDescription :"
-                   +k.getDescription());
-       }
-   }catch(SQLException e){
-       System.out.println("erreur (rechercherDesc)"+e);
-   }
-}
+    public void creationEmploye() {
+        System.out.print("Matricule :");
+        String matr = sc.nextLine();
+        System.out.println("Nom :");
+        String nom = sc.nextLine();
+        System.out.println("Prenom :");
+        String pre = sc.nextLine();
+        System.out.println("Idbur :");
+        int idbur = sc.nextInt();
+        emp = new Employe(0,matr,nom,pre,idbur);
+        try {
+            emp = EmployeDAO.create(emp);
+            System.out.println("\n\tLa valeur de idemp :" + emp.getIdemp() + ""
+                    + "Matricule :" + emp.getMatricule() + ""
+                    + "Nom :" + emp.getNom() + ""
+                    + "Prenom :" + emp.getPrenom() + ""
+                    + "idbur" + emp.getIdbur());
 
+        } catch (SQLException e) {
+            System.out.println("Erreur dans la création (creationEmploye) :" + e);
+        }
+
+    }
+
+///////////////////////////////////////////////////////////////////////////
+    public void rechercherDesc() {
+        System.out.println("Cherchez un bureau sur base de la description :");
+
+        String desc = sc.nextLine();
+        try {
+            List<Bureau> liste = BureauDAO.rechDescription(desc);
+            for (Bureau k : liste) {
+                System.out.println("Idbur :" + k.getIdbur() + ""
+                        + "\tSigle :" + k.getSigle() + ""
+                        + "\tTel :" + k.getTel() + ""
+                        + "\tDescription :"
+                        + k.getDescription());
+            }
+        } catch (SQLException e) {
+            System.out.println("erreur (rechercherDesc)" + e);
+        }
+    }
 
 //////////////////////////////////////////////////////////////////////////
-public void MenuRechercherSigle() throws SQLException{
-    int rep;
-    System.out.print("Sigle que vous voulez trouver : ");
-    String sigle=sc.nextLine();
-    try{
-        bur=BureauDAO.readSigle(sigle);
-    }catch(SQLException e){
-        System.out.println("Impossible de trouver le sigle (MenuRechercherSigle) : "+e);
-    }
-    
-    System.out.println("\nLocal retrouvé !\n\tLa valeur de idbur :"+bur.getIdbur()+
-              "\n\tSigle :"+bur.getSigle()+
-              "\n\tTel :"+bur.getTel()+
-              "\n\tDescription :"+bur.getDescription());
-    int choix;
-    
+    public void MenuRechercherSigle() throws SQLException {
+        int rep;
+        System.out.print("Sigle que vous voulez trouver : ");
+        String sigle = sc.nextLine();
+        try {
+            bur = BureauDAO.readSigle(sigle);
+        } catch (SQLException e) {
+            System.out.println("Impossible de trouver le sigle (MenuRechercherSigle) : " + e);
+        }
+
+        System.out.println("\nLocal retrouvé !\n\tLa valeur de idbur :" + bur.getIdbur()
+                + "\n\tSigle :" + bur.getSigle()
+                + "\n\tTel :" + bur.getTel()
+                + "\n\tDescription :" + bur.getDescription());
+        int choix;
+
         System.out.print("\nEntrer votre choix :"
                 + "\n\t1.Modifier le bureau"
                 + "\n\t2.Supprimer le bureau"
                 + "\n\t3.Retourner au menu");
-        choix=sc.nextInt();
-   sc.skip("\n");
-    
-    switch(choix){
-        
-        case 1: modifierBureau();
-            
-            break;
-            
-        case 2:delete();
-            break;
-        case 3:break;
-    }
-   
-}
-
-public void delete() throws SQLException{
-    try{
-        BureauDAO.delete(bur);
-    }catch(SQLException e){
-        System.out.println("\nerreur de suppression :"+e.getMessage());
-    }
-    menuDebut();
-}
-
-public void modifierBureau() throws SQLException {
-    int choix;
-    do{
-        System.out.println("Modifications : "
-                + "\n\t1.Modifier téléphone"
-                + "\n\t2.Modifier la description"
-                + "\n\t3.Retour au menu précédent\nChoix :");
-        choix=sc.nextInt();
+        choix = sc.nextInt();
         sc.skip("\n");
-        switch(choix){
+
+        switch (choix) {
+
             case 1:
-                System.out.println("Entrer le numéro de téléphone :");
-                String tel=sc.nextLine();
-                bur.setTel(tel);
-                BureauDAO.update(bur);
+                modifierBureau();
+
                 break;
-                
+
             case 2:
-                System.out.println("Entrer la nouvelle description :");
-                String desc=sc.nextLine();
-                bur.setDescription(desc);
-                BureauDAO.update(bur);
-                
+                deleteBur();
+                break;
             case 3:
                 break;
         }
-        
-        
-    }while(choix!=3);
-}
+
+    }
+////////////////////////////////////////////////////////////////////////////
+
+    public void MenuRechercherIdemp() throws SQLException {
+        int rep;
+        System.out.println("Entrer l'idemp que vous recherchez :");
+        int idemp = sc.nextInt();
+        try {
+            emp = EmployeDAO.read(idemp);
+        } catch (SQLException e) {
+            System.out.println("Impossible de trouver l'idemp (MenuRechercherIdemp)" + e);
+
+        }
+
+        System.out.println("\nEmployé retrouvé !\n\tLa valeur de matricule :" + emp.getMatricule()
+                + "Nom :" + emp.getNom()
+                + "Prenom :" + emp.getPrenom()
+                + "Idbur :" + emp.getIdbur());
+        int choix;
+        System.out.println("\nEntrer votre choix :"
+                + "\n\t1.Modifier le client"
+                + "\n\t2.Supprimer le client"
+                + "\n\t3.Retourner au menu");
+        choix = sc.nextInt();
+        sc.skip("\n");
+        switch (choix) {
+            case 1:
+                modifierEmploye();
+                break;
+            case 2:
+                deleteBur();
+                break;
+            case 3:
+                break;
+        }
+
+    }
+////////////////////////////////////////////////////////////////////////////
+
+    public void deleteBur() throws SQLException {
+        try {
+            BureauDAO.delete(bur);
+        } catch (SQLException e) {
+            System.out.println("\nerreur de suppression :" + e.getMessage());
+        }
+        menuDebut();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void deleteEmp() throws SQLException{
+        try{
+            EmployeDAO.delete(emp);
+        }catch(SQLException e){
+             System.out.println("\nerreur de suppression :" + e.getMessage());
+        }
+        menuDebut();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void modifierBureau() throws SQLException {
+        int choix;
+        do {
+            System.out.println("Modifications : "
+                    + "\n\t1.Modifier téléphone"
+                    + "\n\t2.Modifier la description"
+                    + "\n\t3.Retour au menu précédent\nChoix :");
+            choix = sc.nextInt();
+            sc.skip("\n");
+            switch (choix) {
+                case 1:
+                    System.out.println("Entrer le numéro de téléphone :");
+                    String tel = sc.nextLine();
+                    bur.setTel(tel);
+                    BureauDAO.update(bur);
+                    break;
+
+                case 2:
+                    System.out.println("Entrer la nouvelle description :");
+                    String desc = sc.nextLine();
+                    bur.setDescription(desc);
+                    BureauDAO.update(bur);
+
+                case 3:
+                    break;
+            }
+
+        } while (choix != 3);
+    }
+////////////////////////////////////////////////////////////////////////////
+
+    public void modifierEmploye() throws SQLException {
+        int choix;
+        do {
+            System.out.println("Mdifications :"
+                    + "\n\t1.Modifier matricule"
+                    + "\n\t2.Modifier le nom"
+                    + "\n\t3.Modifier le prénom"
+                    + "\n\t4.Retour au menu précédent\nChoix :");
+            choix = sc.nextInt();
+            sc.skip("\n");
+            switch (choix) {
+                case 1:
+                    System.out.println("Entrer le matricule :");
+                    String mat=sc.nextLine();
+                    emp.setMatricule(mat);
+                    EmployeDAO.update(emp);
+                    break;
+                case 2:
+                    System.out.println("Entrer le nom :");
+                    String nom=sc.nextLine();
+                    emp.setNom(nom);
+                    EmployeDAO.update(emp);
+                    break;
+                case 3:
+                    System.out.println("Entrer le prenom :");
+                    String pre=sc.nextLine();
+                    emp.setNom(pre);
+                    EmployeDAO.update(emp);
+                    break;
+                case 4:
+                    break;
+
+            }
+
+        } while (choix != 4);
+    }
+////////////////////////////////////////////////////////////////////////////
+
     public static void main(String[] args) throws SQLException {
-        Projet_Jules projet=new Projet_Jules();
+        Projet_Jules projet = new Projet_Jules();
         projet.menuDebut();
-        
+
     }
 }
