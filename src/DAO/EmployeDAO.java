4e071e8d244f8899adc518c74a1467c886243b4f
billibob <julesblothiaux@gmail.com>
2019-05-08 22:57:02 +0200

    rectification de la classse employé DAO

--- src/DAO/EmployeDAO.java
+++ src/DAO/EmployeDAO.java
@@ -17,17 +17,18 @@ public class EmployeDAO extends DAO<Employe> {
 
     @Override
     public Employe read(int idemp) throws SQLException {
-        String request = "select * from pro_employe where idemp= ?";
+        String request = "select * from PRO_EMPLOYE where idemp= ?";
         try (PreparedStatement pstm = dbConnect.prepareStatement(request)) {
             pstm.setInt(1, idemp);
             try (ResultSet rs = pstm.executeQuery()) {
+                if (rs.next()) {
                 String mat = rs.getString("MATRICULE");
                 String nom = rs.getString("NOM");
                 String pre = rs.getString("PRENOM");
                 int idbur = rs.getInt("IDBUR");
                 return new Employe(idemp, mat, nom, pre, idbur);
-            } catch (SQLException e) {
-                throw e;
+            } else{
+                throw new SQLException("idemp introuvable");
             }
 
         } catch (SQLException e) {
@@ -35,21 +36,23 @@ public class EmployeDAO extends DAO<Employe> {
         }
 
     }
+    }
 
     @Override
     public Employe create(Employe obj) throws SQLException {
         assert obj != null : "L'employe ne peut être null";
         String request1 = "insert into PRO_EMPLOYE(MATRICULE,NOM,PRENOM,IDBUR)values(?,?,?,?)";
-        String request2 = "select idemp from PRO_EMP where MATRICULE=? and NOM=? and PRENOM=? and IDBUR=?";
+        String request2 = "select idemp from PRO_EMPLOYE where MATRICULE=? and NOM=? and PRENOM=? and IDBUR=?";
         try (PreparedStatement pstm1 = dbConnect.prepareStatement(request1);
                 PreparedStatement pstm2 = dbConnect.prepareStatement(request2)) {
             pstm1.setString(1, obj.getMatricule());
             pstm1.setString(2, obj.getNom());
             pstm1.setString(3, obj.getPrenom());
             pstm1.setInt(4, obj.getIdbur());
+            
             int n = pstm1.executeUpdate();
             if (n == 0) {
-                throw new SQLException("Impossible de créer un bureau, aucune ligne insérée");
+                throw new SQLException("Impossible de créer un Employé, aucune ligne insérée");
 
             }
             pstm2.setString(1, obj.getMatricule());
@@ -58,7 +61,7 @@ public class EmployeDAO extends DAO<Employe> {
             try (ResultSet rs = pstm2.executeQuery()) {
                 if (rs.next()) {
                     int idemp = rs.getInt(1);
-                    obj.setIdbur(idemp);
+                    obj.setIdemp(idemp);
                     return read(idemp);
                 } else {
                     throw new SQLException("Impossible de de lire l'idemp");
